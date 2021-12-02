43474c2c061e586e0e689124ad6bc190417785a6
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2021-12-02 14:46:50 -0800

    iteration for time rows but bug in values

--- Develop/script.js
+++ Develop/script.js
@@ -4,4 +4,11 @@ $( "#currentDay" ).append(time);
 
 //timeblocks
 //create rows and columns
+var scheduleArray = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"];
+
+$.each( scheduleArray, function (index, value) {
+    //create div row with div column inside and textcontent value
+    $( "#container" ).append("<div class='row'><div class='time'></div></div>");
+    $( ".time" ).append(value);
+});
 //time on left should stay the same
