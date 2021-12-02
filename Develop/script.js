14ffe576a4c257211c841285b17e0f97d7436dd0
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2021-12-02 15:36:29 -0800

    fixed iteration bug

--- Develop/script.js
+++ Develop/script.js
@@ -7,8 +7,10 @@ $( "#currentDay" ).append(time);
 var scheduleArray = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"];
 
 $.each( scheduleArray, function (index, value) {
-    //create div row with div column inside and textcontent value
-    $( "#container" ).append("<div class='row'><div class='time'></div></div>");
-    $( ".time" ).append(value);
+    var rowDiv = document.createElement("div");
+    var timeDiv = $("<div></div>").text(value);
+    $(timeDiv).attr("id",index);
+    $(rowDiv).appendTo("#container");
+    $(timeDiv).appendTo(rowDiv);
 });
 //time on left should stay the same
