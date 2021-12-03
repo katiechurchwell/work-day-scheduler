18fb11f254457b63240b61f008278b21ca8d6bc3
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2021-12-03 15:03:54 -0800

    color coding, bug with am times larger than pm

--- Develop/script.js
+++ Develop/script.js
@@ -1,28 +1,43 @@
 //current day displayed at top
-var time = moment().format('MMMM Do YYYY');
-$( "#currentDay" ).append(time);
-var currentTime = moment().format('h a'); //need to eventually add minutes
+var time = moment().format("MMMM Do YYYY");
+$("#currentDay").append(time);
+
+//global variables
+var currentTime = String(moment().format("h a"));
 
 //timeblocks
-//create rows and columns
-//business hours
-var scheduleArray = ["9 am","10 am","11 am","12 pm","1 pm","2 pm","3 pm","4 pm","5 pm"];
+var scheduleArray = [
+  "9 am",
+  "10 am",
+  "11 am",
+  "12 pm",
+  "1 pm",
+  "2 pm",
+  "3 pm",
+  "4 pm",
+  "5 pm",
+];
 
 for (let i = 0; i < scheduleArray.length; i++) {
-    var rowDiv = document.createElement("div");
-    var timeDiv = $("<div></div>").text(scheduleArray[i]);
-    $(timeDiv).attr("id",[i]);
-    $(timeDiv).attr("class","time");
-    $(rowDiv).appendTo("#container");
-    $(timeDiv).appendTo(rowDiv);
+  var rowDiv = document.createElement("div");
+  var timeDiv = $("<div></div>").text(scheduleArray[i]);
 
-    // var idTimeDiv = document.getElementById([i]);
-    // if (idTimeDiv > currentTime) {
-    //     $("idTimeDiv").css("background-color", "red");
-    // } else {
-    //     $("idTimeDiv").css("background-color", "blue");
-    // }
-};
+  $(timeDiv).attr("id", [i]);
+  $(rowDiv).appendTo("#container");
+  $(timeDiv).appendTo(rowDiv);
+
+  if (currentTime === scheduleArray[i]) {
+    $(timeDiv).attr("class", "present");
+  }
+
+  if (currentTime > scheduleArray[i]) {
+    $(timeDiv).attr("class", "past");
+  }
+
+  if (currentTime < scheduleArray[i]) {
+    $(timeDiv).attr("class", "future");
+  }
+}
 
 //color coding
 
