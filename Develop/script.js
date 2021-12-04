ebd63a30e4a2d440d68278f2e98924a5d5f9fb70
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2021-12-03 21:25:03 -0800

    lock icons, grid adjustment

--- Develop/script.js
+++ Develop/script.js
@@ -40,9 +40,9 @@ for (let i = 0; i < scheduleArray.length; i++) {
   }
 
   //save button
-  $(saveBtn).attr("class", "saveBtn");
-  $(saveBtn).html('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">'+ '<path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>' + 
-'</svg>')
+  $(saveBtn).attr("class","saveBtn col-1");
+  $(saveBtn).html('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">' + 
+  '<path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>'+'</svg>')
   $(saveBtn).appendTo(rowDiv);
 }
 
